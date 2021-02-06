ee7a170b087630c05554cfb93cb2e435a54c9b8e
Nathan Duckett <nduckett13@gmail.com>
2021-02-06 19:15:34 +1300

    Add comments - Fix check unzip command

--- scripts/one-offs/process-google-music/extract_takeout_contents.sh
+++ scripts/one-offs/process-google-music/extract_takeout_contents.sh
@@ -10,14 +10,20 @@ set +x
 PATH_PREFIX="raw"
 TARGET_PREFIX="extracted"
 
-if [[ -z unzip ]]; then
+# Check the unzip command exists on the system
+if ! command -v unzip; then
     sudo apt install unzip -y
 fi
 
+# Create target directory for output
 mkdir $TARGET_PREFIX
 
+# Loop every zip file in the prefix
 for takeout_folder in $PATH_PREFIX/*.zip; do
+    # Extract the name without .zip at the end
     name=${takeout_folder%.zip}
+    # Extract the name with any path prefixes in front removed
+    # e.g. sub/file becomes file
     folder_name=${name##*/}
     unzip "$takeout_folder" -d "$TARGET_PREFIX/$folder_name"
 done
